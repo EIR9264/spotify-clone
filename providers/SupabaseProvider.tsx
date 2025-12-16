16108f4291fc71d70698a31d76f1f3c32ef065bf
Your Name <EIR_yang@outlook.com>
2025-12-17 05:43:40 +0800

    Providers for auth and Supabase

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Database } from "@/database.types";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+interface SupabaseProviderProps{
+	children:React.ReactNode;
+};
+
+const SupabaseProvider:React.FC<SupabaseProviderProps>=({
+	children
+})=>{
+	const [supabaseClient]=useState(()=>
+		createClientComponentClient()
+	);
+	return (
+		<SessionContextProvider supabaseClient={supabaseClient}>
+			{children}
+		</SessionContextProvider>
+	)
+}
+
+export default SupabaseProvider;
